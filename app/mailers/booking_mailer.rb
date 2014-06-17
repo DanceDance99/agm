54561f7be983141732b6befba88ed5f506f4da4f
Ralitza <funkafaction@gmail.com>
2014-06-17 15:38:43 -0700

    Change mailer email address

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -1,9 +1,9 @@
 class BookingMailer < ActionMailer::Base
-  default from: 'agmecho@yahoo.com'
+  default from: 'agmecho@gmail.com'
 
   def notify_on_new_booking(booking)
     @booking = booking
-    mail(to: 'agmecho@yahoo.com', subject: 'New Shuttle Booking')
+    mail(to: 'agmecho@gmail.com', subject: 'New Shuttle Booking')
   end
 
   def notify_customer_on_new_booking(booking)
--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,10 +1,10 @@
 class ReservationMailer < ActionMailer::Base
-  default from: 'agmecho@yahoo.com'
+  default from: 'agmecho@gmail.com'
 
   def notify_on_new_reservation(tour, reservation)
     @tour = tour
     @reservation = reservation
-    mail(to: 'agmecho@yahoo.com', subject: 'New Tour Reservation')
+    mail(to: 'agmecho@gmail.com', subject: 'New Tour Reservation')
   end
 
   def notify_customer_on_new_reservation(tour, reservation)
