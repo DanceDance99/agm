fcaabd4b9b42252bd02dc0ea37e003ce14652bf3
Ralitza <funkafaction@gmail.com>
2014-06-20 10:21:54 -0700

    changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,9 @@
 //= require jquery
 //= require jquery_ujs
+// require jquery.ui
+//= require jquery.ui.datepicker
 
 //= require jquery.turbolinks
 //= require bootstrap
 
 //= require_tree ./public/
-// require jquery.ui
-//= require jquery.ui.datepicker
