cac1c161af1058388b1ecee1601bf5e7ac62c67d
Ralitza <funkafaction@gmail.com>
2014-07-03 14:53:29 -0700

    settings

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -4,14 +4,12 @@ class ReservationMailer < ActionMailer::Base
   def notify_on_new_reservation(tour, reservation)
     @tour = tour
     @reservation = reservation
-    #attachments.inline['logo.png'] = File.read("#{Rails.root.to_s + '/app/assets/images/logo.png'}")
     mail(to: 'hi@sponsi.com', subject: 'New Tour Reservation')
   end
 
   def notify_customer_on_new_reservation(tour, reservation)
     @tour = tour
     @reservation = reservation
-    #attachments.inline['logo.png'] = File.read("#{Rails.root.to_s + '/app/assets/images/logo.png'}")
     mail(to: @reservation.email_address, subject: 'AGM Tours, Inc. - Tour Booking Confirmation')
   end
 end
