66b86256e79b0470cad1e45e757967b546b22b3c
Ralitza <funkafaction@gmail.com>
2014-07-03 16:00:20 -0700

    settings

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -4,7 +4,7 @@ class BookingMailer < ActionMailer::Base
   def notify_on_new_booking(booking)
     attachments.inline['logo.png'] = File.read("#{Rails.root.to_s + '/app/assets/images/logo.png'}")
     @booking = booking
-    mail(to: 'tourbillontime@live.com', subject: 'New Shuttle Booking')
+    mail(to: 'agmecho@gmail.com', subject: 'New Shuttle Booking')
   end
 
   def notify_customer_on_new_booking(booking)
