924c19f3568cfedbcad65ee8bb9870a38838371b
Ralitza <funkafaction@gmail.com>
2014-07-03 15:38:27 -0700

    settings

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -2,14 +2,14 @@ class BookingMailer < ActionMailer::Base
   default from: 'AGM Tours, Inc. agmecho@gmail.com'
 
   def notify_on_new_booking(booking)
-    @booking = booking
     attachments.inline['logo.png'] = File.read("#{Rails.root.to_s + '/app/assets/images/logo.png'}")
+    @booking = booking
     mail(to: 'agmecho@gmail.com', subject: 'New Shuttle Booking')
   end
 
   def notify_customer_on_new_booking(booking)
-    @booking = booking
     attachments.inline['logo.png'] = File.read("#{Rails.root.to_s + '/app/assets/images/logo.png'}")
+    @booking = booking
     mail(to: @booking.email_address, subject: 'AGM Tours, Inc - Shuttle Booking Confirmation')
   end
 end
--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -2,16 +2,16 @@ class ReservationMailer < ActionMailer::Base
   default from: 'agmecho@gmail.com'
 
   def notify_on_new_reservation(tour, reservation)
+    attachments.inline['logo.png'] = File.read("#{Rails.root.to_s + '/app/assets/images/logo.png'}")
     @tour = tour
     @reservation = reservation
-    attachments.inline['logo.png'] = File.read("#{Rails.root.to_s + '/app/assets/images/logo.png'}")
     mail(to: 'funkafaction@gmail.com', subject: 'New Tour Reservation')
   end
 
   def notify_customer_on_new_reservation(tour, reservation)
+    attachments.inline['logo.png'] = File.read("#{Rails.root.to_s + '/app/assets/images/logo.png'}")
     @tour = tour
     @reservation = reservation
-    attachments.inline['logo.png'] = File.read("#{Rails.root.to_s + '/app/assets/images/logo.png'}")
     mail(to: @reservation.email_address, subject: 'AGM Tours, Inc. - Tour Booking Confirmation')
   end
 end
